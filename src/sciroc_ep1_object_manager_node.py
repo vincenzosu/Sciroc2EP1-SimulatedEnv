c12905b7a25f1930947a74eeaadf9eec2fca757f
Vincenzo Suriani <vincsur@gmail.com>
2021-05-26 15:31:29 +0200

    launchfile

--- src/sciroc_ep1_object_manager_node.py
+++ src/sciroc_ep1_object_manager_node.py
@@ -267,6 +267,8 @@ def change_the_item_srv(req):
         "Requestd object is not on the counter")    
 
     delete_model(old_model_id)
+    print (old_model_id)
+    print (objs_on_tray)
     position_of_old_item = objs_on_tray.index(old_model_id)
     modlist, new_model = load_and_spawn_gazebo_models(obj0, SPAWN_POSES[position_of_old_item])   
     
